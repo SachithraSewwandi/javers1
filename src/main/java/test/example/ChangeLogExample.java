afb4101545248cc24ad922b79fbde4d8db70465c
sachithra <sewwandisachithra@gmail.com>
2015-12-17 17:22:04 +0530

    commit 1

---
+++ src/main/java/test/example/ChangeLogExample.java
@@ -0,0 +1,38 @@
+package test.example;
+
+import org.javers.core.Javers;
+import org.javers.core.JaversBuilder;
+import org.javers.core.changelog.SimpleTextChangeLog;
+import org.javers.core.diff.Change;
+import model.Employee;
+import org.javers.repository.jql.QueryBuilder;
+import org.junit.Test;
+
+import java.util.List;
+
+public class ChangeLogExample {
+
+    @Test
+    public void shoudPrintTextChangeLog() {
+        // given:
+        Javers javers = JaversBuilder.javers().build();
+        Employee bob = new Employee("Bob", 9000, "Scrum master" );
+        javers.commit("hr.manager", bob);
+
+        // do some changes and commit
+        bob.setPosition("Team Lead");
+        bob.setSalary(11000);
+        javers.commit("hr.director", bob);
+
+        bob.addSubordinates(new Employee("Trainee One"), new Employee("Trainee Two"));
+        javers.commit("hr.manager", bob);
+
+        // when:
+        List<Change> changes = javers.findChanges(
+            QueryBuilder.byInstanceId("Bob", Employee.class).build());
+        String changeLog = javers.processChangeList(changes, new SimpleTextChangeLog());
+
+        // then:
+        System.out.println(changeLog);
+    }
+}
